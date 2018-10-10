63dd0aeeb89535203f4beef4e1de0671f751909b
Arthur Bowers <arthurbowers@gmail.com>
2018-10-10 22:59:00 +0100

    Trying to fill goodNameStartings with the first 2 letters of a name and then a list of letters that follow after those 2 letters.

--- Program.cs
+++ Program.cs
@@ -7,20 +7,46 @@ using System.Threading.Tasks;
 namespace nameGenerator {
     class Program {
         static void Main(string[] args) {
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            // Writing names to console.
+            // try {
+            //     for (int i = 0; i < nameAnalyser.names.Count; i++) {
+            //         nameAnalyser.names.ForEach(name => Console.Write("{0}\n", name));
+            //         Console.ReadKey();
+            //     }
+            // } catch (System.IO.IOException nameNull) {
+            //     Console.WriteLine("Exception caught: {0}", nameNull);
+            // } catch (TypeInitializationException typeExcp) {
+            //     Console.WriteLine("Exception caught: {0}", typeExcp);
+            //     Console.ReadKey();
+            // } finally {
+            //     Console.WriteLine("end.");
+            // }
+            List<char> chr = new List<char>();
+            string subStrStart = String.Empty;
+            // Filling the dictionarys.
             try {
-                for (int i = 0; i < nameDictionary.names.Count; i++) {
-                    nameDictionary.names.ForEach(name => Console.Write("{0}\n", name));
-                    Console.ReadKey();
+                for (int i = 0; i < nameAnalyser.names.Count(); i++) {
+                    string chrStart = nameAnalyser.names[i].Substring(2, nameAnalyser.names[i].Length - 2);
+                    subStrStart = nameAnalyser.names[i].Substring(0, 2);
+                    foreach (char c in chrStart) {
+                        chr.Add(c);
+                        
+                    }
+                    
                 }
-            } catch (System.IO.IOException nameNull) {
-                Console.WriteLine("Exception caught: {0}", nameNull);
-            } catch (TypeInitializationException typeExcp) {
-                Console.WriteLine("Exception caught: {0}", typeExcp);
-                Console.ReadKey();
+                
+                nameAnalyser.goodNameStartings.Add(subStrStart, chr);
+            } catch (System.IndexOutOfRangeException outOfRange) {
+                Console.WriteLine("Exception caught: {0}", outOfRange);
+            } catch (System.ArgumentException argExcp) {
+                Console.WriteLine("Exception caught: {0},", argExcp);
             } finally {
                 Console.WriteLine("end.");
             }
+
+            Console.WriteLine("Values: " + nameAnalyser.goodNameStartings.Values);
+            Console.WriteLine("Keys:  " + nameAnalyser.goodNameStartings.Keys);
+            Console.ReadKey();
             
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
