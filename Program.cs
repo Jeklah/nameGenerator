be4f71263e60c05c9f8f98818c7c42d7ad611d3a
Arthur Bowers <arthurbowers@gmail.com>
2018-10-10 20:11:02 +0100

    Fixed initial errors by making dictionary and the used variables public static, also more efficient way of creating a list using the construction template of passing it an array.

--- Program.cs
+++ Program.cs
@@ -9,7 +9,9 @@ namespace nameGenerator {
         static void Main(string[] args) {
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             
-            
+            for (int i = 0; i < nameDictionary.names.Count;) {
+
+            }
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
---
+++ nameDictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace nameGenerator {
+    public class nameDictionary {
+        const string FILEPATH = "D:\\Projects\nameGenerator\nameGenerator\names.txt";
+        // List<string> names = new List<string>();
+        static string[] nameFile = File.ReadAllLines(FILEPATH);
+        public List<string> names = new List<string>(nameFile); 
+    }
+}
