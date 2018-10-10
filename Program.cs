f3ed3fc37bfe6b39f9d70d214bf7f999e7e661b4
Arthur Bowers <arthurbowers@gmail.com>
2018-10-10 19:42:31 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nameGenerator {
+    class Program {
+        static void Main(string[] args) {
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            
+            
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
